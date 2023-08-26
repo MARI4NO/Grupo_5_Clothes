3c7aba923c3fbec3c00a4813194c02ac0c4ebf0c
JulietaNatalia <137336104+JulietaNatalia@users.noreply.github.com>
2023-08-26 10:27:17 -0300

    Update mainController.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,26 +1,39 @@
 const path=require("path");
+const headerView = "header"; 
+const footerView = "footer";
 const controller={
     home: (req, res)=>{
-        res.render("index");
+        res.render("index", { header: headerView, footer: footerView });
     },
     login:(req, res)=>{
-        res.render("login")
+        res.render("login", { header: headerView, footer: footerView });
     },
     register:(req, res)=>{
-        res.render("register")
+        res.render("register", { header: headerView, footer: footerView });
     },
     misTickets:(req, res)=>{
-        res.render("myTickets")
-    },
-    misTickets:(req, res)=>{
-        res.render("myTickets")
+        res.render("myTickets", { header: headerView, footer: footerView });
     },
     miCarrito:(req, res)=>{
-        res.render("productCart")
+        res.render("productCart", { header: headerView, footer: footerView });
     },
     detalleProducto:(req, res)=>{
-        res.render("productDetail")
+        res.render("productDetail", { header: headerView, footer: footerView });
+    }
+    create: (req, res) => {
+        res.render("create", { header: headerView, footer: footerView });
+    },
+    edit: (req, res) => {
+        res.render("edit", { header: headerView, footer: footerView });
     }
 };
 
 module.exports=controller;
+
+
+
+
+
+
+
+
