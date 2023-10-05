96a556535cdd80a33bc797d7f708c44a5f24f3c4
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-10-05 19:44:17 -0300

    fix errors

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const productRouter = express.Router();
 
 // Configuración de multer
-const upload = require("../config/multer.config");
+const { uploadProduct } = require("../config/multer.config");
 
 //importando controlador
 const productController = require("../controllers/productController");
@@ -12,13 +12,13 @@ productRouter.get("/products/:id/detail", productController.detalleProducto);
 productRouter.get("/products/create", productController.create);
 productRouter.post(
     "/products",
-    upload.single("image"),
+    uploadProduct.single("image"),
     productController.storeProduct
 );
 productRouter.get("/products/:id/edit", productController.edit);
 productRouter.put(
     "/products/:id",
-    upload.single("image"),
+    uploadProduct.single("image"),
     productController.update
 );
 productRouter.delete("/delete/:id", productController.destroy);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const userRouter = express.Router();const upload = require("../config/multer.config");
+const userRouter = express.Router();
+const { uploadUser } = require("../config/multer.config");
 const usersController = require("../controllers/userController");
 
 userRouter.get("/login", usersController.loginView);
 userRouter.post("/login", usersController.login);
 userRouter.get("/register", usersController.registerView);
-userRouter.post("/register", upload.single("image"), usersController.register);
+userRouter.post(
+    "/register",
+    uploadUser.single("image"),
+    usersController.register
+);
 userRouter.get("/misTickets", usersController.misTickets);
 userRouter.get("/miCarrito", usersController.miCarrito);
 
