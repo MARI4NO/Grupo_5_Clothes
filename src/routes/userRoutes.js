175e2b320469699fea9717ca8a2d8e1e5074374a
JulietaNatalia <137336104+JulietaNatalia@users.noreply.github.com>
2023-10-05 19:14:42 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,49 +1,5 @@
 const express = require("express");
-const userRouter = express.Router();
-
-// Configuración de multer
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, "uploads"));
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
-});
-const upload = require("../config/multer.config");
-
-// Ruta para manejar el registro de usuarios
-app.post("/register", upload.single("image"), async (req, res) => {
-    try {
-        // Obtener datos del formulario
-        const { firstName, lastName, email, password } = req.body;
-
-        // Encriptar la contraseña
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Construir el objeto de usuario
-        const user = {
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-            image: req.file.filename, // Nombre del archivo subido
-        };
-
-        // Guardar datos en el archivo JSON
-        const users = JSON.parse(fs.readFileSync("users.json"));
-        users.push(user);
-        fs.writeFileSync("users.json", JSON.stringify(users));
-
-        // Redirigir a la página de inicio 
-        res.redirect("/home");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error en el servidor");
-    }
-});
-
-//importando controlador
+const userRouter = express.Router();const upload = require("../config/multer.config");
 const usersController = require("../controllers/userController");
 
 userRouter.get("/login", usersController.loginView);
