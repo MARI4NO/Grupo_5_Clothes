7e51a5117fd29fcc78f6f0cc30ac11a9ddd118dd
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-30 17:32:37 -0300

    agregar controlador para listar los productos

--- src/api/controllers/products.controller.js
+++ src/api/controllers/products.controller.js
@@ -1,5 +1,32 @@
+const db = require("../../database/models");
+const url = require("../config/url.public");
+
 const productsAPIController = {
-    list: async (req, res) => {},
+    list: async (req, res) => {
+        db.Products.findAll()
+            .then((events) => {
+                console.log(events.length);
+
+                const productsMap = events.map((event) => ({
+                    id: event.id,
+                    title: event.title,
+                    city: event.city,
+                    place: event.place,
+                    address: event.address,
+                    type: event.type,
+                    price: event.price,
+                    availables: event.availables,
+                    image: `${url.URL_PUBLIC_IMAGES_PRODUCTS}/${event.image}`,
+                    detail: `${url.URL_API}/products/${event.id}`,
+                }));
+
+                return res.json({
+                    count: events.length,
+                    products: productsMap,
+                });
+            })
+            .catch((err) => res.json(err));
+    },
     detail: async (req, res) => {},
 };
 
