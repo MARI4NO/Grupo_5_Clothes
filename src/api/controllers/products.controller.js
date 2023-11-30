fa2e0235a379ff59b1e3ed7aa3bcbf4fcff2c127
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-30 17:38:40 -0300

    agregar controlador para mostrar el detalle de un producto

--- src/api/controllers/products.controller.js
+++ src/api/controllers/products.controller.js
@@ -2,7 +2,7 @@ const db = require("../../database/models");
 const url = require("../config/url.public");
 
 const productsAPIController = {
-    list: async (req, res) => {
+    list: (req, res) => {
         db.Products.findAll()
             .then((events) => {
                 console.log(events.length);
@@ -27,7 +27,25 @@ const productsAPIController = {
             })
             .catch((err) => res.json(err));
     },
-    detail: async (req, res) => {},
+    detail: (req, res) => {
+        const { id } = req.params;
+
+        db.Products.findByPk(id)
+            .then((event) => {
+                res.json({
+                    id: event.id,
+                    title: event.title,
+                    city: event.city,
+                    place: event.place,
+                    address: event.address,
+                    type: event.type,
+                    price: event.price,
+                    availables: event.availables,
+                    image: `${url.URL_PUBLIC_IMAGES_PRODUCTS}/${event.image}`,
+                });
+            })
+            .catch((err) => res.json(err));
+    },
 };
 
 module.exports = productsAPIController;
